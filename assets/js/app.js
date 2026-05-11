8067e6be03f63d64c37ba618b91d6909c3b4f543
William Burkert <willburkert96@gmail.com>
2026-05-11 15:22:51 +0100

    added copy to clipboard mechanic using clipboard api

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,11 @@
 // Variables
 const btn = document.getElementById('btn');
+const symbolCheck = document.getElementById('symbol-check');
+const copy = document.getElementById('copy-btn')
+let copied = document.getElementById('copied')
 let password = document.getElementById('generated-password');
+
+// Word Lists
 const animals = [
     "Dog", "Cat", "Rabbit", "Elephant", "Lion", "Tiger", "Zebra", "Giraffe", "Monkey", "Horse",
     "Cow", "Pig", "Sheep", "Goat", "Chicken", "Duck", "Frog", "Turtle", "Penguin", "Koala",
@@ -31,21 +36,49 @@ const colors = [
 ]
 
 const objects = [
-            "Car", "Bike", "Chair", "Table", "Computer", "Phone", "Book", "Bag", "Shoe", "Ball",
-            "Clock", "Flower", "Tree", "Lamp", "Pen", "Guitar", "Camera", "Cup", "Knife", "Fork",
-            "Spoon", "Plate", "Glass", "Key", "Door", "Window", "House", "Boat", "Plane", "Rocket",
-            "Moon", "Star", "Sun", "Cloud", "Mountain", "River", "Lake", "Ocean", "Bridge", "Road",
-            "Train", "Bus", "Bicycle", "Motorcycle", "Cylinder", "Cube", "Curve", "Hexagon", "Heart",
-            "Line", "Octagon", "Oval", "Plane", "Prism", "Pyramid", "Sphere", "Sector", "Square",
-            "Triangle", "Bottle", "Water", "Watch", "Stamp", "Umbrella", "Pencil", "Lighter", "Passport",
-            "Notebook", "Laptop", "Mirror", "Battery", "Pillow", "Fridge", "Sofa", "Sink", "Whiteboard",
-            "Helmet", "Sweater", "Jumper", "Coat", "Scarf", "Backpack", "Calculator", "Folder", "Paper"
-        ]
+    "Car", "Bike", "Chair", "Table", "Computer", "Phone", "Book", "Bag", "Shoe", "Ball",
+    "Clock", "Flower", "Tree", "Lamp", "Pen", "Guitar", "Camera", "Cup", "Knife", "Fork",
+    "Spoon", "Plate", "Glass", "Key", "Door", "Window", "House", "Boat", "Plane", "Rocket",
+    "Moon", "Star", "Sun", "Cloud", "Mountain", "River", "Lake", "Ocean", "Bridge", "Road",
+    "Train", "Bus", "Bicycle", "Motorcycle", "Cylinder", "Cube", "Curve", "Hexagon", "Heart",
+    "Line", "Octagon", "Oval", "Plane", "Prism", "Pyramid", "Sphere", "Sector", "Square",
+    "Triangle", "Bottle", "Water", "Watch", "Stamp", "Umbrella", "Pencil", "Lighter", "Passport",
+    "Notebook", "Laptop", "Mirror", "Battery", "Pillow", "Fridge", "Sofa", "Sink", "Whiteboard",
+    "Helmet", "Sweater", "Jumper", "Coat", "Scarf", "Backpack", "Calculator", "Folder", "Paper"
+]
 
-const symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '?', '+', '=', '~'];      
+const symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '?', '+', '=', '~'];
+
+
+
 
 btn.addEventListener('click', function () {
-    password.textContent = '';
+    // Resets the CSS animation 
     void password.offsetWidth;
-    password.textContent = animals[Math.floor(Math.random() * animals.length)] + colors[Math.floor(Math.random() * colors.length)] + objects[Math.floor(Math.random() * objects.length)] + symbols[Math.floor(Math.random() * symbols.length)];
+    // Clears the copy to clipboard text displayed on the DOM
+    copied.textContent = ''
+    // Resets the password box when pressing the button a second time
+    password.textContent = '';
+
+    // Symbol Check 
+    if (symbolCheck.checked) {
+        return password.textContent = animals[Math.floor(Math.random() * animals.length)] + colors[Math.floor(Math.random() * colors.length)] + objects[Math.floor(Math.random() * objects.length)] + symbols[Math.floor(Math.random() * symbols.length)]
+    } else {
+
+        return password.textContent = animals[Math.floor(Math.random() * animals.length)] + colors[Math.floor(Math.random() * colors.length)] + objects[Math.floor(Math.random() * objects.length)];
+    }
+
+    
 })
+
+
+copy.addEventListener('click', function(){
+    navigator.clipboard.writeText(password.textContent)
+    .then(function() {
+        copied.textContent = "Copied to clipboard";
+
+    })
+})
+
+
+
