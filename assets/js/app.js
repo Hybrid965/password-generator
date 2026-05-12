3f8e1a7b0a16828b4bf5fbedf30befafd9f5207d
William Burkert <willburkert96@gmail.com>
2026-05-12 08:19:31 +0100

    added character and number check

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,8 @@
 // DOM Variables
 const btn = document.getElementById('btn');
 const symbolCheck = document.getElementById('symbol-check');
+const char = document.getElementById('char-check')
+const num = document.getElementById('num-check')
 const copy = document.getElementById('copy-btn')
 let copied = document.getElementById('copied')
 let password = document.getElementById('generated-password');
@@ -47,6 +49,12 @@ const objects = [
     "Helmet", "Sweater", "Jumper", "Coat", "Scarf", "Backpack", "Calculator", "Folder", "Paper"
 ]
 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+    61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
+    81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
+
 const symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '?', '+', '=', '~'];
 
 //Random Function
@@ -56,7 +64,7 @@ function getRandom(arr) {
 
 // Event Listener for Generate Password
 btn.addEventListener('click', function () {
-    
+
     // Clears the copy to clipboard text displayed on the DOM
     copied.textContent = ''
     // Resets the password box when pressing the button a second time
@@ -67,12 +75,25 @@ btn.addEventListener('click', function () {
 
     let newPassword = getRandom(animals) + getRandom(objects) + getRandom(colors);
 
+    //Number Check
+    if (num.checked) {
+        newPassword = getRandom(animals) + getRandom(numbers) + getRandom(objects) + getRandom(numbers) + getRandom(colors) + getRandom(numbers);
+    }
+
     // Symbol Check 
     if (symbolCheck.checked) {
         newPassword += getRandom(symbols)
     }
-        password.textContent = newPassword
-    
+
+    // Character check
+    if (char.checked && newPassword.length < 15) {
+        newPassword += getRandom(animals)
+    }
+
+
+
+    password.textContent = newPassword
+
 
 })
 // Event Listener for Copy button
